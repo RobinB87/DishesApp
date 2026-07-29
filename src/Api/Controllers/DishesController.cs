53abdd10d1473a2d22c57c9e1be1842a74c472ec
Robin Bette <robinbette@hotmail.com>
2026-07-29 21:12:09 +0200

    Setup new project with DishesController

---
+++ src/Api/Controllers/DishesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DishesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetDishes()
+        {
+            // Logic to retrieve dishes from the database or service
+            var dishes = new List<string> { "Spaghetti", "Pizza", "Salad" };
+            return Ok(dishes);
+        }
+    }
+}
