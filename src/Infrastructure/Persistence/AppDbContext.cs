573ee9607fa3de1ec6f78773edc83b113d73bf55
Robin Bette <robinbette@hotmail.com>
2026-08-01 13:49:39 +0200

    add efcore and setup db context

---
+++ src/Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,37 @@
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+
+    public DbSet<Dish> Dishes { get; set; }
+    public DbSet<Ingredient> Ingredients { get; set; }
+    public DbSet<DishIngredient> DishIngredients { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Dish>()
+            .HasIndex(d => d.Name)
+            .IsUnique();
+
+        modelBuilder.Entity<Ingredient>()
+            .HasIndex(i => i.Name)
+            .IsUnique();
+
+        modelBuilder.Entity<DishIngredient>()
+            .HasKey(di => new { di.DishId, di.IngredientId });
+
+        modelBuilder.Entity<DishIngredient>()
+            .HasOne(di => di.Dish)
+            .WithMany(dish => dish.DishIngredients)
+            .HasForeignKey(di => di.DishId);
+
+        modelBuilder.Entity<DishIngredient>()
+            .HasOne(di => di.Ingredient)
+            .WithMany(ingredient => ingredient.DishIngredients)
+            .HasForeignKey(di => di.IngredientId);
+    }
+}
---
+++ src/Infrastructure/README.md
@@ -0,0 +1,27 @@
+# Setup
+
+cd into src/Infrastructure and then install the following:
+
+## EntityFrameworkCore for PostgreSQL
+```bash
+dotnet add package Npgsql.EntityFrameworkCore.PostgreSQL --version 10.0.3
+```
+
+!! install the following in src/Api:
+
+To be able to run migrations, also required:
+```bash
+dotnet add package Microsoft.EntityFrameworkCore.Design --version 10.0.10
+```
+
+## CLI for migrations
+```bash
+dotnet tool install --global dotnet-ef
+```
+
+### Run first migration
+Run from the root of the repository, not from within this Infrastructure location:
+
+```bash
+dotnet ef migrations add InitialCreate --project src/Infrastructure --startup-project src/Api
+```
