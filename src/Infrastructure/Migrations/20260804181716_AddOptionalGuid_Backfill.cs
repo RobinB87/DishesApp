38f8786c4b3da283bc3bdbee073379980afca81f
Robin Bette <robinbette@hotmail.com>
2026-08-04 20:20:11 +0200

    feat(idempotency): backfill guid for existing dish and ingredient rows

---
+++ src/Infrastructure/Migrations/20260804181716_AddOptionalGuid_Backfill.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOptionalGuid_Backfill : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                UPDATE "Dishes" SET "Guid" = gen_random_uuid() WHERE "Guid" IS NULL;
+                """);
+
+            migrationBuilder.Sql(
+                """
+                UPDATE "Ingredients" SET "Guid" = gen_random_uuid() WHERE "Guid" IS NULL;
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // No-op: reverting a backfill by nulling guids back out isn't meaningful.
+        }
+    }
+}
