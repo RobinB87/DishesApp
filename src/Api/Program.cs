00c5f275bec771e01a053803a5d490c5eaea8275
Robin Bette <robinbette@hotmail.com>
2026-08-08 12:19:59 +0200

    feat(logging): setup seq

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,5 +1,12 @@
 using Application;
 using Infrastructure;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.Seq("http://localhost:5341")
+    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
 
